0d4d43366f77aad87605f6f6bdad19a068037bb1
jasimn <113772434+jasimn@users.noreply.github.com>
2025-07-17 00:09:55 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "myec2_instance" {
     ami = "ami-09e6f87a47903347c"
     instance_type = "t2.micro"
@@ -6,4 +18,7 @@ resource "aws_instance" "myec2_instance" {
     }
   
 }
+
+
+
 
